e548c5e952ad7a2043dda0365753aab41ee1484f
Frédérick Duberville <frederick@MacBook-Pro-de-Frederick.local>
2017-04-03 08:57:06 -0400

    Add Db logic to display and add tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,45 @@
-import React, { Component } from 'react';
-
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { Mongo } from 'meteor/mongo';
 import Task from './Task.jsx';
 import Comment from './Comment.jsx';
+import { Tasks } from '../api/tasks.js';
+import { createContainer } from 'meteor/react-meteor-data';
 
 function Item(props) {
   return <li>{props.message}</li>;
 }
 
 // App component - represents the whole app
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    hideCompleted: false,
+  };
+}
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  toggleHideCompleted() {
+  this.setState({
+    hideCompleted: !this.state.hideCompleted,
+  });
+}
+
   getTasks() {
     return [
       { _id: 1, text: 'This is task 1', state:'doing' },
@@ -32,6 +63,22 @@ export default class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+            <label className="hide-completed">
+              <input
+                type="checkbox"
+                readOnly
+                checked={this.state.hideCompleted}
+                onClick={this.toggleHideCompleted.bind(this)}
+               />
+               Hide Completed Tasks
+            </label>
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+           <input
+             type="text"
+             ref="textInput"
+             placeholder="Type to add new tasks"
+           />
+         </form>
         </header>
 
         <ul>
@@ -46,3 +93,13 @@ export default class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+return {
+  tasks: Tasks.find({}).fetch(),
+};
+}, App);
